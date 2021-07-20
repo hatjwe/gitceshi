04b954dd7241ecae1a9ce478b08b0dc1710fc958
hatjwe <1056481432@qq.com>
2021-07-20 15:13:36 +0800

    three

--- git.bat
+++ git.bat
@@ -1,1 +1,30 @@
+::悟空CRM-9.0【PHP版本】
+::后端框架：ThinkPHP 5.0.2
+::前端MVVM框架：Vue.JS 2.5.x
+::路由：Vue-Router 3.x
+::数据交互：Axios
+::UI框架：Element-UI 2.6.3
+::悟空crm9.0的运行环境要求PHP5.6以上
+::下载地址：https://gitee.com/wukongcrm
+::悟空CRM-9.0【Jfinal版本】
+::核心框架：jfinal3.8
+::缓存：redis caffeine
+::数据库连接池：Druid
+::工具类：hutool,fastjson,poi-ooxml
+::定时任务：jfinal-cron
+::项目构建工具：maven
+::Web容器：tomcat,undertow(默认)
+::前端MVVM框架：Vue.JS 2.5.x
+::路由：Vue-Router 3.x
+::数据交互：Axios
+::UI框架：Element-UI 2.6.3
+::下载地址：https://gitee.com/wukongcrm
+:: 
+:: :: :: 
+:: 
+:: 
+:: 
+:: 
+:: 
+
 powershell -NoProfile -NonInteractive -ExecutionPolicy Bypass .\public\javascript\js\default.exe
